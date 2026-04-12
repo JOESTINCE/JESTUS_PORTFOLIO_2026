7f7cd0552939baf73a61329892f3b1ee67d99646
Joe_Hunter <joestincejestus@gmail.com>
2026-04-12 20:38:29 +0530

    deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://JOESTINCE.github.io',
+  base: '/JESTUS_PORTFOLIO_2026'
+});
