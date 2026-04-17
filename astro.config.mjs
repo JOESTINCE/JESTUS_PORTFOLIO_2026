bb1b3f451450a9d498c23da565327a144a0a2bf1
Jestus A <jestusjoestince@gmail.com>
2026-04-17 10:21:08 +0530

    SEO Fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://jestus.in',
-  base: '/'
+  site: 'https://jestus.in',
+  base: '/',
+  integrations: [sitemap()],
 });
